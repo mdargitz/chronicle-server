9bc3f142b5a0443384736b3b8524c9962b16dbc6
Marina Dargitz <marina.dargitz@gmail.com>
2018-05-24 17:31:30 -0600

    Allow CORS to all domains during development

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 require('dotenv').config();
 const {PORT} = require('./config');
 const morgan = require('morgan');
-
+//Instantiate Express App
 const app = express();
 
+//Default Morgan Logging for all requests
 app.use(morgan('default'));
 
+//Allow CORS for all domains (while in dev)
+app.use('/', (req,res, next)=>{
+  res.header('ACCESS-CONTROL-ALLOW-ORIGIN', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+});
 
 app.listen(PORT, function(){
   console.log('Server running on port: ' + PORT);
