0f10bfd5d7ee69dd6d7419d1eeb376a70d2fee35
Marina Dargitz <marina.dargitz@gmail.com>
2018-05-24 17:26:47 -0600

    Add morgan logging on default settings

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const {PORT} = require('./config');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('default'));
+
 
 app.listen(PORT, function(){
   console.log('Server running on port: ' + PORT);
