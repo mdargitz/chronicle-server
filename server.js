1d56fc836507110720ff0dcd6be96315013bbd16
Marina Dargitz <marina.dargitz@gmail.com>
2018-05-24 17:37:41 -0600

    Mount routers and create router skeletons

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 require('dotenv').config();
 const {PORT} = require('./config');
 const morgan = require('morgan');
+
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
+const storyRouter = require('./routes/story');
+const characterRouter = require('./routes/character');
+const settingRouter = require('./routes/setting');
+const plotRouter = require('./routes/plot');
+
 //Instantiate Express App
 const app = express();
 
@@ -13,6 +21,14 @@ app.use('/', (req,res, next)=>{
   res.header('ACCESS-CONTROL-ALLOW-ORIGIN', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 });
+
+//All Routes
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+app.use('/api/stories', storyRouter);
+app.use('/api/characters', characterRouter);
+app.use('/api/settings', settingRouter);
+app.use('/api/plots', plotRouter);
 
 app.listen(PORT, function(){
   console.log('Server running on port: ' + PORT);
