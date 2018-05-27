37c3facf864728454b7a9cf4abd60ead6f769064
Marina Dargitz <marina.dargitz@gmail.com>
2018-05-26 19:38:54 -0600

    Add put endpoint to character router

--- routes/character.js
+++ routes/character.js
@@ -15,7 +15,51 @@ router.get('/:storyId', (req, res, next) => {
   knex('characters')
     .select()
     .where({user_id, story_id})
-    .then(result => res.json(result));
+    .then(result => res.json(result))
+    .catch(err => next(err));
+});
+
+router.get('/:storyId/:id', (req, res, next)=>{
+  const user_id = req.user.id;
+  const story_id = req.params.storyId;
+  const {id} = req.params;
+
+  knex('characters')
+    .select()
+    .where({user_id, story_id, id})
+    .then(result => {
+      if (result[0]){
+        return res.json(result[0]);
+      }
+      return next();
+    })
+    .catch(err => next(err));
+});
+
+router.put('/:storyId/:id', (req, res, next)=>{
+  const user_id = req.user.id;
+  const story_id = req.params.storyId;
+  const {id} = req.params;
+
+  const updateableFields = ['name', 'age', 'occupation', 
+    'description', 'personality', 'background'];
+
+  const updatedCharacter = {};
+
+  updateableFields.map(field => {
+    if(field in req.body){
+      updatedCharacter[field] = req.body[field];
+    }
+  });
+
+  knex('characters')
+    .where({user_id, story_id, id})
+    .update(updatedCharacter)
+    .returning(['id', 'story_id', 'name'])
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
 });
 
 router.post('/:storyId', (req, res, next)=>{
